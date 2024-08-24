51b6730580ebd8329cf220073aa1753f3be07ab3
Kenny Strawn <kstrawn0@saddleback.edu>
2024-08-24 16:37:51 -0700

    Fix Python

--- cloud.nix
+++ cloud.nix
@@ -9,7 +9,38 @@
     ec2stepshell
     gato
     gcp-scanner
-    ggshield
+    (ggshield.overrideAttrs(_: rec {
+      build-system = with python311Packages; [ setuptools ];
+
+      dependencies = with python311Packages; [
+        appdirs
+        charset-normalizer
+        click
+        cryptography
+        marshmallow
+        marshmallow-dataclass
+        oauthlib
+        platformdirs
+        pygitguardian
+        pyjwt
+        python-dotenv
+        pyyaml
+        requests
+        rich
+      ];
+
+      nativeCheckInputs =
+      [ git ]
+      ++ (with python311Packages; [
+        jsonschema
+        pyfakefs
+        pytest-mock
+        pytest-voluptuous
+        pytestCheckHook
+        snapshottest
+        vcrpy
+      ]);
+    }))
     goblob
     imdshift
     pacu
--- docs/nix-security-tool-box.nix
+++ docs/nix-security-tool-box.nix
@@ -179,7 +179,38 @@
     gdb
     gef
     genymotion
-    ggshield
+    (ggshield.overrideAttrs(_: rec {
+      build-system = with python311Packages; [ setuptools ];
+
+      dependencies = with python311Packages; [
+        appdirs
+        charset-normalizer
+        click
+        cryptography
+        marshmallow
+        marshmallow-dataclass
+        oauthlib
+        platformdirs
+        pygitguardian
+        pyjwt
+        python-dotenv
+        pyyaml
+        requests
+        rich
+      ];
+
+      nativeCheckInputs =
+      [ git ]
+      ++ (with python311Packages; [
+        jsonschema
+        pyfakefs
+        pytest-mock
+        pytest-voluptuous
+        pytestCheckHook
+        snapshottest
+        vcrpy
+      ]);
+    }))
     ghauri
     ghidra-bin
     ghost
