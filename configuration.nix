48506f55995faf1b3cbfc798430b01677a8e9cb4
Kenny Strawn <kstrawn0@saddleback.edu>
2024-08-24 16:53:46 -0700

    Fix Python

--- configuration.nix
+++ configuration.nix
@@ -13,7 +13,7 @@ in
       
       # Always spoof user agent to fix the problem of curl having a hard time
       # downloading certain files
-      fetchurl = prev.fetchurl.overrideAttrs(_: {
+      final.fetchurl = prev.fetchurl.overrideAttrs(_: {
         curlOptsList = [
           "-HUser-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
           "-L"
